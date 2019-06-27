e80981dd08933935dee9936d76a3243d387699ca
Abhishek Bera <abhisecre@gmail.com>
2019-06-27 20:37:37 +0530

    Added social oauth

--- authman/urls.py
+++ authman/urls.py
@@ -1,7 +1,12 @@
 from django.conf import urls
-from django.urls import path
+from django.urls import path, include
 from . import views
+from django.contrib.auth.views import LogoutView
+from django.conf import settings
 
 urlpatterns = [
-    path("", views.login_page, name="login_page")
+    path("", views.login_page, name="login"),
+    path('auth', include('social_django.urls', namespace='social')),
+    path("home", views.login_page, name="home"),
+    path('logout', LogoutView.as_view(),{'next_page': settings.LOGOUT_REDIRECT_URL}, name = "logout")
 ]
--- authman/views.py
+++ authman/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def login_page(request):
     return render(request, "login.html")
+
+def home_page(request):
+    return render(request, "home.html")
